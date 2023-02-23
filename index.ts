90873f2a9ffe0c5f5391139c61f011394ac6246d
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-02-23 20:50:50 -0300

    WIP

--- index.ts
+++ index.ts
@@ -1,31 +1,78 @@
 import assert from "assert";
+import { createWriteStream, WriteStream } from "fs";
 import _ from "lodash";
 import fetch from "node-fetch";
 import accents from "remove-accents";
 
+const TEMPLATE_BEGIN = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<array>`;
+
+const TEMPLATE_END = `</array>
+</plist>`;
+
+function write(stream: WriteStream, content: string) {
+  return new Promise<void>((resolve, reject) => {
+    stream.write(content + "\n", (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
+async function create(i: number) {
+  const stream = createWriteStream(
+    `dist/Text-Substitutions.${i}.plist`,
+    "utf-8"
+  );
+  await write(stream, TEMPLATE_BEGIN);
+  return stream;
+}
+
+async function close(stream: WriteStream) {
+  await write(stream, TEMPLATE_END);
+  await new Promise<void>((resolve, reject) => {
+    stream.close((err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
 (async () => {
   const resp = await fetch("https://www.ime.usp.br/~pf/dicios/br-utf8.txt");
   const words = (await resp.text()).split(`\n`);
-  assert(words.pop() === "");
-  console.log(`<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<array>`);
+  let i = 0;
+  let fileIndex = 0;
+  if (_.last(words) === "") {
+    assert(words.pop() === "");
+  }
+  let stream = await create(0);
   const wordsMap = _.groupBy(words, (word) => accents.remove(word));
-  for (const pureWord in wordsMap) {
+  const keys = Object.keys(wordsMap).sort((a, b) => a.localeCompare(b));
+  for (const pureWord of keys) {
     if (wordsMap[pureWord].length !== 1) {
       continue;
     }
     if (!accents.has(wordsMap[pureWord][0])) {
       continue;
     }
-    console.log(`\t<dict>
+    i++;
+    await write(
+      stream,
+      `\t<dict>
 \t\t<key>phrase</key>
 \t\t<string>${wordsMap[pureWord]}</string>
 \t\t<key>shortcut</key>
 \t\t<string>${pureWord}</string>
-\t</dict>`);
+\t</dict>`
+    );
+    if (i === 1000) {
+      i = 0;
+      await close(stream);
+      stream = await create(++fileIndex);
+    }
   }
-  console.log(`</array>
-</plist>`);
+  await close(stream);
 })();
