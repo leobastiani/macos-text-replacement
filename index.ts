56fbeddd6fdee85491dea8113c3a9494c9b5e1c4
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-06-23 13:00:18 -0300

    add sao exception

--- index.ts
+++ index.ts
@@ -16,6 +16,7 @@ matches:`);
 
   // exceptions
   wordsMap["amanha"] = ["amanhã"];
+  wordsMap["sao"] = ["são"];
   // remove some words to write English
   delete wordsMap.audio;
   delete wordsMap.has;
